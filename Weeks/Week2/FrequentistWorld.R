ae02979ef73f79ee84be1c28545a26b2c09f689f
Leo <leonardorejorge@gmail.com>
2023-10-18 14:02:01 +0200

    correct p value calculation

--- Weeks/Week2/FrequentistWorld.R
+++ Weeks/Week2/FrequentistWorld.R
@@ -3,12 +3,12 @@ library(rethinking)
 ## A randomization-based frequentist approach (just because this is not a gaussian problem)
 
 ## function to toss a globe covered by p by water N times
-sim_globe <- function(p = 0.7, N = 10){
+sim_globe <- function(p = 0.7, N = 9){
    sample(c("W", "L"), size = N, prob = c(p, 1-p), replace = TRUE)
 }
 
-(sample <- sim_globe())
-(p_bar <- sum(sample == "W")/length(sample))
+(sample <- sim_globe(p = 0.7, N = 9))
+(p_bar <- sum(sample == "W"))
 
 ## all of the frequentist statistics is based on repeated sampling. If I would go back to the field and 
 ## sample infinite times following my null hypothesis, what is the probability I'd get such an extreme value?
@@ -17,12 +17,15 @@ sim_globe <- function(p = 0.7, N = 10){
 
 (null_samples <- t(replicate(sim_globe(p = 0.5), n = 1e4)))
 
-null_dist <- apply(null_samples, MARGIN = 1, FUN = function(x){sum(x == "W")/length(x)}, simplify = T)
+null_dist <- apply(null_samples, MARGIN = 1, FUN = function(x){sum(x == "W")}, simplify = T)
 
-dens(null_dist, lwd = 2)
+simplehist(null_dist, lwd = 5, round = F)
 abline(v = p_bar, lwd = 2, col = 2)
 
-
-p_value <- min(sum(null_dist > p_bar)/length(null_dist), 
-               sum(null_dist < p_bar)/length(null_dist))*2
+p_diff <- p_bar - mean(null_dist)
+p_value <- sum(null_dist > mean(null_dist) + p_diff)/length(null_dist) + sum(null_dist < mean(null_dist) - p_diff)/length(null_dist)
 p_value
+
+## Usando pbinom
+pbinom(q = mean(null_dist) + p_diff, prob = 0.5, size = 9, lower.tail = F) + pbinom(q = mean(null_dist) - p_diff, prob = 0.5, size = 9, lower.tail = T)
+
